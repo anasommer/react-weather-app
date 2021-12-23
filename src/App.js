b628fcb6935d2f7889cc33dccf12248e8a4520dd
anasommer <anastassia.sommer@gmail.com>
2021-12-23 21:02:20 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="http://anasommer.com" target="_blank" rel="noreferrer">
+          Ana Sommer
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/anasommer/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced in Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
