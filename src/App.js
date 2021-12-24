ea3ade6bd3106b77df9e7a61c9530c7bcc1ad7cd
anasommer <anastassia.sommer@gmail.com>
2021-12-24 17:03:43 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="http://anasommer.com" target="_blank" rel="noreferrer">
-          Ana Sommer
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/anasommer/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced in Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="http://anasommer.com" target="_blank" rel="noreferrer">
+            Ana Sommer
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/anasommer/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced in Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
